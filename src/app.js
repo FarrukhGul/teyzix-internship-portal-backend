72cc14296f8aa379f50a8dcf7916c15887ca217e
FarrukhGul <coder.gulfarrukh@gmail.com>
2026-05-17 04:22:49 +0500

    front end url fixed problem

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ const app = express();
 // Middlewares
 app.use(helmet())
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+  origin: process.env.FRONTEND_URL || 'https://teyzix-internship-portal-frontend.onrender.com',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
